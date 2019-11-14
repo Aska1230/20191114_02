b76382fda085cc084a8485493c6ededa7366078f
aska1230 <joanna.slewa@gmail.com>
2019-11-14 14:28:16 +0100

    second drop

---
+++ wretyu.py
@@ -0,0 +1,22 @@
+print('Hello world')
+
+print('464'.isalpha())
+print('sdff'.isalpha())
+print('464'.isdigit())
+print('sddsd'.isdigit())
+
+
+def p(a):
+
+        if a%2 == 0:
+            print('Yes')
+        else:
+            print('No')
+p(2)
+
+(age, size) = "56,345".split(',')
+print(age)
+print(size)
+(2, 4, 5)
+
+print('ssss')
